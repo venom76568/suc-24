41d27df313b009f225ddfa31a907a741305ec870
Maitruu <tmaitreya2004@gmail.com>
2023-10-24 22:32:19 +0530

    added navbar and background

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {BrowserRouter} from "react-router-dom";
+import Home from "./components/Home"
+
+const App = () => {
+    return(
+        <BrowserRouter>
+        <Home/>
+        </BrowserRouter>
+        
+
+    );
+};
+export default App;
