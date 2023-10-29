81743cdad06eb0dba06ffe52a50446d5251c4508
Maitruu <tmaitreya2004@gmail.com>
2023-10-30 01:08:18 +0530

    today

--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -4,7 +4,7 @@ import './Timeline.css'
 const TimeLine = () => (
 <div>
     <section>
-    <div className="bg-black text-white py-8">
+    <div className="bg-opacity-0 text-white py-8">
     <div className="container mx-auto flex flex-col items-start md:flex-row my-12 md:my-12">
       <div className="flex flex-col w-full sticky md:top-36 lg:w-1/3 mt-2 md:mt-12 px-8">
         <p className="ml-2 text-yellow-400 uppercase tracking-loose">TIMELINE</p>
@@ -17,14 +17,14 @@ const TimeLine = () => (
         Explore Now</a> */}
       </div>
       <div className="ml-0 md:ml-12 lg:w-2/3 sticky">
-        <div className="container mx-auto w-full h-full">
+        <div className="bg-opacity-0 container mx-auto w-full h-full">
         <div id="Dot1"><span id="dot1"></span></div>
-          <div className="relative wrap overflow-hidden p-10 h-full">
+          <div className=" bg-opacity-0 relative wrap overflow-hidden p-10 h-full">
             <div className="style1 border-2-2 border-yellow-555 absolute h-full border"
               ></div>
             <div className="style2 border-2-2 border-yellow-555 absolute h-full border"
               ></div>
-            <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
+            <div className="bg-opacity-0 mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
               <div className="order-1 w-5/12"></div>
               <div className="order-1 w-5/12 px-1 py-4 text-right">
                 <p className="mb-3 text-base text-yellow-400">7 Nov, 2023</p>
@@ -55,7 +55,7 @@ const TimeLine = () => (
               </div>
             </div>
 
-            <div className="mb-8 flex justify-between items-center w-full right-timeline">
+            <div className="bg-opacity-0 mb-8 flex justify-between items-center w-full right-timeline">
               <div className="order-1 w-5/12"></div>
 
               <div className="order-1  w-5/12 px-1 py-4">
@@ -68,7 +68,7 @@ const TimeLine = () => (
             </div>
           </div>
           <div id="Dot1"><span id="dot1"></span></div>
-          <img className="mx-auto -mt-36 md:-mt-36" src="https://user-images.githubusercontent.com/54521023/116968861-ef21a000-acd2-11eb-95ac-a34b5b490265.png" alt='hello' />
+          <img className="bg-opacity-0 mx-auto -mt-36 md:-mt-36" src="https://user-images.githubusercontent.com/54521023/116968861-ef21a000-acd2-11eb-95ac-a34b5b490265.png" alt='hello' />
         </div>
       </div>
     </div>
